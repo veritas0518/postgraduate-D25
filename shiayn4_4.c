f14b8a72f1e9085591820335a041e48ab6a841a2
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-05-20 15:15:57 +0800

    A question about the partners in the queue
    【问题描述】假设在周末舞会上，男士们和女士们进入舞厅时，各自排成一队。跳舞开始时，依次从男队和女队的队头各出一人配成舞伴。若两队初始人数不相同，则较长的那一队中未配对者等待下一轮舞曲。现要求写一算法模拟上述舞伴配对问题。
    
    【输入形式】
    
    先输入一个正整数T,接下来T组数据。
    每组数据分为两部分，名字与性别。1代表男士，0代表女士。
    如：Tom 1代表一位名叫Tom的男士。
    
    【输出形式】
    
    输出较长的那一队中队首的未配对者的姓名。
    若两队人数相同，输出No one is waiting!

---
+++ shiayn4_4.c
@@ -0,0 +1,32 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//typedef  struct  Node
+//{
+//    char  name[50];//姓名
+//    int  sex;//性别，1代表男士，0代表女士。
+//    struct  Node* next;
+//}  person;
+//
+//typedef  struct
+//{
+//    person* front;
+//    person* rear;
+//}  Queue;
+//
+//void  pair_dancers(Queue* mp, Queue* wp)//mp代表男士队，wp代表女士队
+//{
+//    int T;
+//    scanf("%d", &T);
+//}
+//
+//int  main(void)
+//{
+//    Queue* mp, * wp;
+//    mp = (Queue*)malloc(sizeof(Queue));
+//    mp->front = mp->rear = NULL;
+//    wp = (Queue*)malloc(sizeof(Queue));
+//    wp->front = wp->rear = NULL;
+//    pair_dancers(mp, wp);
+//    return  0;
+//}
